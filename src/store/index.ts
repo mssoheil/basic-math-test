9c641b77096c0d4eb10b96ec80eeda4b46cc3814
mssoheil88 <mssoheil88@yahoo.com>
2019-12-23 12:25:41 +0330

    feat: redux devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,19 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 // reducers
 import reducers from "./index.reducer";
+import middlewares from "./index.middleware";
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
-const appStore = createStore(reducers, {});
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const appStore = createStore(
+  reducers,
+  {},
+  composeEnhancer(applyMiddleware(...middlewares))
+);
 
 export default appStore;
